46815f3b430c2b5fc83e7b2e0b9a101cfaf8c88b
Santosh Rana <pysantosh.np@gmail.com>
2018-09-18 16:30:47 +0545

    All category model is register on admin panel

--- category/admin.py
+++ category/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import BookInstance, Book, Genre, Author
 
-# Register your models here.
+
+admin.site.register(Book)
+admin.site.register(Genre)
+admin.site.register(Author)
+admin.site.register(BookInstance)
