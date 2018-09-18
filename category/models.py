a8438fb021d909cff23fe69d47d771bc1f8fb315
Santosh Rana <pysantosh.np@gmail.com>
2018-09-18 16:04:05 +0545

    Author model created

--- category/models.py
+++ category/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        ordering = ['first_name', 'last_name']
+
+    def get_absolute_url(self):
+        """Returns the url to access a particular author instance."""
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return f'{self.last_name}, {self.first_name}'
