b7bba15e01a0cc605a1834c987b917481454cf09
Santosh Rana <pysantosh.np@gmail.com>
2018-09-20 15:01:44 +0545

    static files serving setting added

--- epustakalaya/urls.py
+++ epustakalaya/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('category/', include('category.urls')),
     path('', RedirectView.as_view(url='/category/')),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
